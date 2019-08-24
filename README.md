a15ede319d120cdb8cae17b2234b688c930a46c1
Hornwitser <github@hornwitser.no>
2019-08-25 00:45:02 +0200

    AntiHTML
    The simple stupid data structure oriented HTML serializer.

---
+++ README.md
@@ -0,0 +1,40 @@
+AntiHTML
+========
+
+The simple stupid data structure oriented HTML serializer.
+
+Simple exaple
+-------------
+
+```js
+const a = require('antihtml');
+
+let document = a.htmlDocument(['html',
+    ['head',
+        ['meta', {'charset': 'utf-8'}],
+        ['title', a.Text("Hello world")],
+    ],
+    ['body',
+        ['h1', a.Text("Hello world")],
+        ['p', a.Text("This is a sample html document")],
+    ]
+]);
+```
+
+Produces a string containing an html document roghly equivalent to the
+following, with the exception of there being no newlines or extra
+whitespace:
+
+```html
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Hello world</title>
+    </head>
+    <body>
+        <h1>Hello world</h1>
+        <p>This is a sample html document</p>
+    </body>
+</html>
+```
---
+++ antihtml.js
@@ -0,0 +1,237 @@
+"use strict";
+
+class Node { }
+
+// Imaginary node for the serializer
+class _HTMLRoot extends Node {
+    constructor() {
+        super();
+        this.nodes = [];
+    }
+}
+
+// Represents a DOCTYPE
+class _HTMLDocumentType extends Node {
+    constructor(name) {
+        super();
+        this.name = name;
+    }
+}
+
+// Represents a Text node
+class _HTMLText extends Node {
+    constructor(data) {
+        super();
+        this.data = data;
+    }
+}
+
+// Represents a comment
+class _HTMLComment extends Node {
+    constructor(data) {
+        super();
+        this.data = data;
+    }
+}
+
+// Represents an Element
+class _HTMLElement extends Node {
+    constructor(name) {
+        super();
+        this.attributes = new Map();
+        this.name = name;
+        this.nodes = [];
+    }
+}
+
+
+function _escape_attribute(value) {
+    value = value.replace(/&/g, '&amp;')
+    value = value.replace(/\xA0/g, '&nbsp;') // NO-BREAK SPACE
+    value = value.replace(/"/g, '&quot;')
+    return value
+}
+
+function _escape_node(value) {
+    value = value.replace(/&/g, '&amp;')
+    value = value.replace(/\xA0/g, '&nbsp;') // NO-BREAK SPACE
+    value = value.replace(/</g, '&lt;')
+    value = value.replace(/>/g, '&gt;')
+    return value
+}
+
+function _serialize_attributes(attributes) {
+    return [...attributes.entries()].map(([name, value]) => {
+        return [' ', name, '="', _escape_attribute(value), '"'].join('')
+    }
+    ).join('');
+}
+
+function _serializes_as_void(node) {
+    return [
+        // Void elements
+        'area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'link',
+        'meta', 'param', 'source', 'track', 'wbr',
+
+        // Also serializes as void
+        'basefont', 'bgsound', 'frame', 'keygen',
+    ].includes(node.name);
+}
+
+function _serialize(node) {
+    // Note: see https://html.spec.whatwg.org/#serialising-html-fragments
+    //       for the reference algorithm for serializing HTML.  For
+    //       simplicity some features are left out here.
+
+    if (node instanceof _HTMLElement && _serializes_as_void(node)) {
+        return '';
+    }
+
+    if (node instanceof _HTMLDocumentType || node instanceof _HTMLText) {
+        return '';
+    }
+
+    let s = [];
+
+    for (let child of node.nodes) {
+        if (child instanceof _HTMLElement) {
+            let attrs = child.attributes;
+            s.push('<', child.name, _serialize_attributes(attrs), '>');
+
+            if (_serializes_as_void(child)) {
+                continue;
+            }
+
+            s.push(_serialize(child));
+            s.push('</', child.name, '>');
+
+        } else if (child instanceof _HTMLText) {
+            if (node instanceof _HTMLElement && [
+                'style', 'script', 'xmp', 'iframe', 'noembed',
+                'noframes', 'plaintext',
+            ].includes(node.name)) {
+                // Dissalow pontentially bad things altogether.  This will not
+                // catch all bad things unless adjecant text nodes are merged.
+
+                if (child.data.indexOf('<!--') !== -1) {
+                    throw new Error("<!-- in text preserving element");
+                }
+
+                if (child.data.indexOf('<' + node.name) !== -1) {
+                    throw new Error("opening tag in text preserving element");
+                }
+
+                if (child.data.indexOf('</' + node.name) !== -1) {
+                    throw new Error("closing tag in text preserving element");
+                }
+
+                s.push(child.data);
+
+            } else {
+                s.push(_escape_node(child.data));
+            }
+
+        } else if (child instanceof _HTMLComment) {
+            if (child.data.indexOf('-->') !== -1) {
+                throw Error("Comment containing -->");
+            }
+
+            s.push('<!--', child.data, '-->');
+
+        } else if (child instanceof _HTMLDocumentType) {
+            s.push('<!DOCTYPE ', child.name, '>');
+
+        } else {
+            throw Error("unsupported node "+child);
+        }
+    }
+
+    return s.join('');
+}
+
+function Text(content) {
+    let text = Object.create(Text.prototype);
+    text.content = content;
+    return text;
+}
+
+function Comment(content) {
+    let comment = Object.create(Comment.prototype);
+    comment.content = content;
+    return comment;
+}
+
+function _html(tag) {
+    if (!(tag instanceof Array)) {
+        throw new Error("tag must be an Array");
+    }
+
+    let type = tag.shift();
+
+    if (typeof type !== 'string') {
+        throw new Error("first element in tag must be a string");
+    }
+
+    let element = new _HTMLElement(type);
+    for (let item of tag) {
+        if (typeof item === 'string') {
+            let classAttr = element.attributes.get('class');
+            if (classAttr !== undefined) {
+                classAttr += " "+item;
+            } else {
+                classAttr = item;
+            }
+
+            element.attributes.set('class', classAttr);
+
+        } else if (item instanceof Text) {
+            element.nodes.push(new _HTMLText(item.content));
+
+        } else if (item instanceof Comment) {
+            element.nodes.push(new _HTMLComment(item.content));
+
+        } else if (item instanceof Array) {
+            element.nodes.push(_html(item));
+
+        } else if (item === null) {
+            // ignore nulls for ease of conditinal objects
+
+        } else {
+            let prototype = Object.getPrototypeOf(item);
+
+            if (prototype === Object.prototype || prototype === null) {
+                for (let key in item) {
+                    element.attributes.set(key, item[key]);
+                }
+
+            } else {
+                throw new Error("Unsupported content type "+typeof item);
+            }
+        }
+    }
+
+    return element;
+}
+
+function htmlFragment(tag) {
+    let root = new _HTMLRoot();
+    root.nodes = [_html(tag)];
+    return _serialize(root);
+}
+
+function htmlDocument(...tags) {
+    let root = new _HTMLRoot();
+    root.nodes = [new _HTMLDocumentType('html'), ...tags.map(_html)];
+    return _serialize(root);
+}
+
+module.exports =  {
+    Text,
+    Comment,
+    htmlFragment,
+    htmlDocument,
+
+    // For testing only
+    _html,
+    _serialize,
+};
